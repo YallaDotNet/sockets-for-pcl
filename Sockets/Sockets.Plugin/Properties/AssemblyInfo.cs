6e7809ea738d14f75d9ab46830578ac418b8bdde
Dmitry Shechtman <damnian@gmail.com>
2015-12-11 07:55:22 +0200

    Fix Sockets.Plugin AssemblyInfo

--- Sockets/Sockets.Plugin/Properties/AssemblyInfo.cs
+++ Sockets/Sockets.Plugin/Properties/AssemblyInfo.cs
@@ -1,15 +1,11 @@
 ﻿using System.Reflection;
 using System.Resources;
+using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Sockets.Plugin")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-
-[assembly: AssemblyVersion("1.3.0")]
-[assembly: AssemblyFileVersion("1.3.0")]
+[assembly: AssemblyTitle("Sockets.Plugin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
